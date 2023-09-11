2b32e6917f9e3cf993dfcbfce914e192e7667b73
DT Modern Tech <dtmoderntech@gmail.com>
2023-09-11 20:14:24 +0500

    initial commit

---
+++ database/migrations/2023_09_11_140422_create_settings_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSettingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('settings', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->boolean('private_account')->default(0);
+            $table->boolean('secure_payment')->default(0);
+            $table->boolean('sync_contact_no')->default(0);
+            $table->boolean('app_notification')->default(0);
+            $table->string('language')->default('english');
+            $table->timestamps();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->index('user_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('settings');
+    }
+}
