579a9bfa1c24c90b07e6fc18990220abe4d11fa1
rickeysxhemz <rickeysxhemz@gmail.com>
2023-09-14 09:29:27 -0400

    sept 14 work on admin authentication

---
+++ database/migrations/2023_09_13_113149_create_o_t_p_s_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOTPSTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('o_t_p_s', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->integer('otp_value')->nullable();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('o_t_p_s');
+    }
+}
