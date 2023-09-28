4ac94d2b4943a0a1d2ea42475421e70b0711bfd4
rickeysxhemz <rickeysxhemz@gmail.com>
2023-09-28 08:54:50 -0400

    phone issue resolved

--- config/auth.php
+++ config/auth.php
@@ -92,7 +92,7 @@ return [
         'users' => [
             'provider' => 'users',
             'table' => 'password_resets',
-            'expire' => 60,
+            'expire' => 60*24*30,
             'throttle' => 60,
         ],
     ],
