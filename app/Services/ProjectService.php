b865b5c8f002edb7042cff18103b41e2736cd24d
rickeysxhemz <rickeysxhemz@gmail.com>
2023-10-16 16:07:11 -0400

    migration and comment work

--- app/Services/ProjectService.php
+++ app/Services/ProjectService.php
@@ -18,6 +18,7 @@ use App\Models\ProjectImage;
 use App\Http\Traits\CommonTrait;
 use App\Models\Message;
 use App\Models\Conversation;
+use App\Models\ProjectComment;
 class ProjectService extends BaseService
 {
     use CommonTrait;
@@ -152,6 +153,22 @@ class ProjectService extends BaseService
         try{
            
                     DB::beginTransaction();
+                    $comment_exist=ProjectComment::where('project_id',$request->project_id)->first();
+                    if($comment_exist)
+                    {
+                        $exists=ProjectComment::find($comment_exist->id);
+                        $exists->unread_comments_count=$exists->unread_comments_count+1;
+                        $exists->read=false;
+                        $exists->save();
+                    }else
+                    {
+                    $project_comment = new ProjectComment();
+                    $project_comment->project_id = $request->project_id;
+                    $project_comment->user_id = auth()->user()->id;
+                    $project_comment->read = false;
+                    $project_comment->unread_comments_count = 1;
+                    $project_comment->save();
+                    }
                     $comment = new Comment();
                     $comment->sender_id = auth()->user()->id;
                     // $comment->receiver_id = $request->receiver_id;
@@ -254,6 +271,23 @@ class ProjectService extends BaseService
             DB::rollback();
             $error = "Error: Message: " . $e->getMessage() . " File: " . $e->getFile() . " Line #: " . $e->getLine();
             Helper::errorLogs("ProjectService: getProjects", $error);
+            return false;
+        }
+    }
+    public function readComment($request)
+    {
+        try{
+            DB::beginTransaction();
+            $project_comment=ProjectComment::where('project_id',$request->project_id)->first();
+            $project_comment->unread_comments_count=0;
+            $project_comment->read=true;
+            $project_comment->save();
+            DB::commit();
+            return $project_comment;
+        }catch(Exception $e){
+            DB::rollback();
+            $error = "Error: Message: " . $e->getMessage() . " File: " . $e->getFile() . " Line #: " . $e->getLine();
+            Helper::errorLogs("ProjectService: readComment", $error);
             return false;
         }
     }
